75f541e342c0bc723ab31a6328bc9b80016c99af
FarhanShafqut <farhanshafqut869@gmail.com>
2023-08-12 16:27:01 +0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
+import Header from '@/components/Header/Header'
+import Fotter from '@/components/Fotter/Fotter'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Header/>
+    <div className='container mx-auto min-h-screen' >
+  <Component {...pageProps} />
+  </div>
+  <Fotter/>
+  </>)
 }
